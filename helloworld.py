835e247aaaa646e12b462ab67044c838b1dc6956
Coasternaut <48424921+Coasternaut@users.noreply.github.com>
2024-05-30 00:22:42 -0400

    basic button

--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,9 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+def clicked():
+    print("clicked")
+
 
 def window():
     app = QApplication(sys.argv)
@@ -12,6 +15,10 @@ def window():
     label = QtWidgets.QLabel(win)
     label.setText("Hello World")
     label.move(50, 50)
+    
+    b1 = QtWidgets.QPushButton(win)
+    b1.setText("Click me")
+    b1.clicked.connect(clicked)
     
     win.show()
     sys.exit(app.exec_())
