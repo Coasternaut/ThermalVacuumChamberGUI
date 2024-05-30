8ad6076f4f73640e53dd638ec9b660c96c4661c9
Coasternaut <48424921+Coasternaut@users.noreply.github.com>
2024-05-30 00:33:21 -0400

    reorganized to class

--- helloworld.py
+++ helloworld.py
@@ -2,25 +2,40 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+class window(QMainWindow):
+    def __init__(self):
+        super(window, self).__init__()
+        self.setGeometry(100, 200, 400, 200)
+        self.setWindowTitle("Hello World")
+        self.initUI()
+        
+    
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Hello World")
+        self.label.move(50, 50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me")
+        self.b1.clicked.connect(self.clicked)
+        
+    def clicked(self):
+        self.label.setText("you pressed the button")
+        self.update()
+        
+    def update(self):
+        self.label.adjustSize()
+
+
 def clicked():
     print("clicked")
 
 
-def window():
+def app():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(100, 200, 400, 200)
-    win.setWindowTitle("Hello World")
-    
-    label = QtWidgets.QLabel(win)
-    label.setText("Hello World")
-    label.move(50, 50)
-    
-    b1 = QtWidgets.QPushButton(win)
-    b1.setText("Click me")
-    b1.clicked.connect(clicked)
-    
+    win = window()
+
     win.show()
     sys.exit(app.exec_())
     
-window()
+app()
