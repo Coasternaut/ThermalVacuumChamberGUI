98100f149badf3cd930cc2b2ea87933818cb4d65
Coasternaut <48424921+Coasternaut@users.noreply.github.com>
2024-05-30 00:20:31 -0400

    basic hello page

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(100, 200, 400, 200)
+    win.setWindowTitle("Hello World")
+    
+    label = QtWidgets.QLabel(win)
+    label.setText("Hello World")
+    label.move(50, 50)
+    
+    win.show()
+    sys.exit(app.exec_())
+    
+window()
