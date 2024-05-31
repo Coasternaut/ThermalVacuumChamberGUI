bfe6f29fa5da3c5a3352b971cd4de430eb37fe40
Coasternaut <48424921+Coasternaut@users.noreply.github.com>
2024-05-31 16:07:37 -0400

    dials work

--- demoTVCgui.py
+++ demoTVCgui.py
@@ -1,11 +1,21 @@
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QDial
 import sys
 
 class mainApp(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("demoTVCgui.ui", self)
+        self.presDial.actionTriggered.connect(self.setPres)
+        self.tempDial.actionTriggered.connect(self.setTemp)
+        
+    def setPres(self):
+        currentPres = self.presDial.value()
+        self.currentPres.setText(str(currentPres))
+        
+    def setTemp(self):
+        currentTemp = self.tempDial.value()
+        self.currentTemp.setText(str(currentTemp))
 
 if __name__ == '__main__':
     app = QApplication([])
