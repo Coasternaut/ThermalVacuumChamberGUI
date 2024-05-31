c5fbe0d97a368471f57196c2954912ef31719a56
Coasternaut <48424921+Coasternaut@users.noreply.github.com>
2024-05-31 15:06:28 -0400

    base layout of demo TVG gui

---
+++ demoTVCgui.py
@@ -0,0 +1,13 @@
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication
+
+Form, Window = uic.loadUiType("demoTVCgui.ui")
+
+app = QApplication([])
+window = Window()
+
+form = Form()
+form.setupUi(window)
+
+window.show()
+app.exec()
