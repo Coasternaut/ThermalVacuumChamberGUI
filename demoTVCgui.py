a659adcf793dbeab0d18463cbd04f165de2b58ff
Coasternaut <48424921+Coasternaut@users.noreply.github.com>
2024-05-31 17:21:50 -0400

    working graph demo

--- demoTVCgui.py
+++ demoTVCgui.py
@@ -1,5 +1,6 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow, QDial
+from PyQt6.QtCore import QTimer
 import pyqtgraph as pg
 import sys
 
@@ -9,13 +10,20 @@ class mainApp(QMainWindow):
         uic.loadUi("demoTVCgui.ui", self)
         self.presDial.actionTriggered.connect(self.setPres)
         self.tempDial.actionTriggered.connect(self.setTemp)
-        self.logButton.pressed.connect(self.logData)
+        self.startButton.pressed.connect(self.startLogging)
+        self.stopButton.pressed.connect(self.stopLogging)
         
         self.presLog = []
         self.tempLog = []
         
         self.currentPres = 0
         self.currentTemp = 0
+        self.elapsedTime = 0
+        
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.logData)
+        
+        self.refreshInterval = self.refreshBox.value()
         
     def setPres(self):
         self.currentPres = self.presDial.value()
@@ -26,6 +34,8 @@ class mainApp(QMainWindow):
         self.currentTempDisp.setText(str(self.currentTemp))
         
     def logData(self):
+        self.elapsedTime += self.refreshInterval
+        self.elapsedTimeDisplay.setText(str(self.elapsedTime))
         self.presLog.append(self.currentPres)
         self.tempLog.append(self.currentTemp)
         self.plotData()
@@ -33,6 +43,13 @@ class mainApp(QMainWindow):
     def plotData(self):
         self.presPlot.plot(self.presLog, pen="r")
         self.tempPlot.plot(self.tempLog, pen="b")
+        
+    def startLogging(self):
+        self.refreshInterval = self.refreshBox.value()
+        self.timer.start(self.refreshInterval)
+    
+    def stopLogging(self):
+        self.timer.stop()
 
 if __name__ == '__main__':
     app = QApplication([])
