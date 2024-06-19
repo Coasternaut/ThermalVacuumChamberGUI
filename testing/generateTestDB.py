d91d6d5b0993a2b6402dbae0e12f13f401618044
Coasternaut <48424921+Coasternaut@users.noreply.github.com>
2024-06-18 23:16:22 -0400

    fixed test generator

--- testing/generateTestDB.py
+++ testing/generateTestDB.py
@@ -8,7 +8,7 @@ dbPath = f'testDB_{size}.db'
     
 db = sqlite3.connect(dbPath, check_same_thread=False)
 db.execute("CREATE TABLE temp_log(timestamp, tempA, tempB, tempC, tempD, tempE, tempF, tempG)")
-db.execute("CREATE TABLE chiller_log(timestamp, bath_temp, pump_pres, setpoint)")
+db.execute("CREATE TABLE chiller_log(timestamp, bath_temp, pump_pres, temp_setpoint)")
 
 for i in range(size):
     timestamp += 1
@@ -21,7 +21,7 @@ for i in range(size):
                             round(random.uniform(0.0, 100.0), 2),
                             round(random.uniform(0.0, 100.0), 2),
                             round(random.uniform(0.0, 100.0), 2)))
-    db.execute("INSERT INTO chiller_log(timestamp, bath_temp, pump_pres, setpoint) VALUES (?, ?, ?, ?)",
+    db.execute("INSERT INTO chiller_log(timestamp, bath_temp, pump_pres, temp_setpoint) VALUES (?, ?, ?, ?)",
                (timestamp,
                 round(random.uniform(0.0, 100.0), 2),
                 round(random.uniform(0.0, 100.0), 2),
