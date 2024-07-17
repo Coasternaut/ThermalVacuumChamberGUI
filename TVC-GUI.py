39c2c80f652da3ca48a16f4c2c09a442c30267a2
Coasternaut <ben.m.jordan@icloud.com>
2024-07-17 15:21:23 -0400

    fixed UnboundLocalError

--- TVC-GUI.py
+++ TVC-GUI.py
@@ -391,6 +391,7 @@ def writeSerialData(serialDevice, dataString):
             return False
         
 def requestSerialData(serialDevice, requestString):
+    data = None
     try:
         serialDevice.connectionObject.reset_input_buffer()
         serialDevice.connectionObject.write(bytes(requestString, 'ascii'))
