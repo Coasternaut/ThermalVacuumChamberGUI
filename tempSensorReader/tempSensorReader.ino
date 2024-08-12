6bc542c92868b08bcd6b3a0f93a944be18e37a87
Ben Jordan <bmj@udel.edu>
2024-08-12 17:10:45 -0400

    cleaned up Arduino code

--- tempSensorReader/tempSensorReader.ino
+++ tempSensorReader/tempSensorReader.ino
@@ -9,19 +9,11 @@ int input = 0;
 
 bool outputValues = false;
 
-const int LOOP_DELAY_MS = 10;
+const int LOOP_DELAY_MS = 15;
 
-const int READS_PER_CYCLE = 5;
+const int READS_PER_CYCLE = 10;
 
-const int AVERAGE_POINTS = 300;
-
-//movingAvg avgSensorA(averagePoints);
-//movingAvg avgSensorB(averagePoints);
-//movingAvg avgSensorC(averagePoints);
-//movingAvg avgSensorD(averagePoints);
-//movingAvg avgSensorE(averagePoints);
-//movingAvg avgSensorF(averagePoints);
-//movingAvg avgSensorG(averagePoints);
+const int AVERAGE_POINTS = 400;
 
 movingAvg sensorAvgs[NUM_SENSORS] = {movingAvg(AVERAGE_POINTS),
                                      movingAvg(AVERAGE_POINTS), 
@@ -50,26 +42,7 @@ void loop() {
     if (input == 68) { // ASCII for "D"
       outputValues = true;
     }
-    if (input == 82) { // ASCII for "R"  prints raw read average
-      for (int i = 0; i < NUM_SENSORS; i++) {
-        Serial.print(sensorAvgs[i].getAvg());
-        Serial.print(';');
-      }
-      Serial.println('\r');
-    }
-    if (input == 65) { // ASCII for "A" prints 5 readings from Sensor A ADC
-      for (int i = 0; i < 5; i++) {
-        Serial.println(analogRead(TEMP_PINS[0]));
-      }
-    }
-//    if (input == 77) { // ASCII for "M" prints readings in millivolts
-//      for (int i = 0; i < NUM_SENSORS; i++) {
-//        Serial.print(convert_mV(sensorAvgs[i].getAvg()));
-//        Serial.print(';');
-//      }
-//      Serial.println('\r');
-//    }
-
+    
    input = 0;
  }
 
@@ -78,15 +51,14 @@ void loop() {
 
       // adds new reading to rolling average
       for (int j = 0; j < READS_PER_CYCLE; j++) {
-        sensorAvgs[i].reading(ADC_to_Celsius100(analogRead(TEMP_PINS[i])));
+        sensorAvgs[i].reading(ADC_to_mCelsius(analogRead(TEMP_PINS[i])));
       }
       
       // prints temperature data if requested
       if (outputValues) {
-        Serial.print((sensorAvgs[i].getAvg()) / 100.0);
+        Serial.print((sensorAvgs[i].getAvg()) / 1000.0, 1);
         Serial.print(';');
       }
-
     }
 
   // finishes line if printing and resets output flag
@@ -98,24 +70,9 @@ void loop() {
   // limits loop speed
   delay(LOOP_DELAY_MS);
 }
-//
-//float convertCelsius(int mV) {
-//  //float mV = reading * (AREF_VOLTAGE / 1024.0);
-//  // Serial.print(mV);
-//  // Serial.println(" millivolts");
-//
-//  float celsius = (mV - 500) / 10.0;
-//  // Serial.print(celsius);
-//  // Serial.println(" degrees C");
-//
-//  return celsius;
-//}
 
-//long convert_mV(int reading) {
-//   (reading * AREF_mV) / 1024;
-//}
-
-int ADC_to_Celsius100(int reading) {
+// converts an ADC reading to Celsius * 1000
+int ADC_to_mCelsius(int reading) {
   long uV = (reading * AREF_uV) / 1024;
-  return (uV - 500000) / 100;
+  return (uV - 500000) / 10;
 }
