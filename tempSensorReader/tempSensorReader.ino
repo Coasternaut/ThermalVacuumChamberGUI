74def94740f8d4bf37051b7004041fb521c91d27
Ben Jordan <bmj@udel.edu>
2024-08-12 16:05:50 -0400

    averages adc reading

--- tempSensorReader/tempSensorReader.ino
+++ tempSensorReader/tempSensorReader.ino
@@ -1,44 +1,99 @@
+#include <movingAvg.h>
+
 const uint8_t TEMP_PINS[] = {A2, A0, A1, A3, A5, A6, A4};
 const int NUM_SENSORS = 7;
 
-const float aref_voltage = 3.285;
+const float AREF_VOLTAGE = 3.285;
 
 int input = 0;
+
+bool outputValues = false;
+
+const int LOOP_DELAY_MS = 10;
+
+const int READS_PER_CYCLE = 5;
+
+const int AVERAGE_POINTS = 300;
+
+//movingAvg avgSensorA(averagePoints);
+//movingAvg avgSensorB(averagePoints);
+//movingAvg avgSensorC(averagePoints);
+//movingAvg avgSensorD(averagePoints);
+//movingAvg avgSensorE(averagePoints);
+//movingAvg avgSensorF(averagePoints);
+//movingAvg avgSensorG(averagePoints);
+
+movingAvg sensorAvgs[NUM_SENSORS] = {movingAvg(AVERAGE_POINTS),
+                                     movingAvg(AVERAGE_POINTS), 
+                                     movingAvg(AVERAGE_POINTS), 
+                                     movingAvg(AVERAGE_POINTS),
+                                     movingAvg(AVERAGE_POINTS), 
+                                     movingAvg(AVERAGE_POINTS),
+                                     movingAvg(AVERAGE_POINTS)};
 
 void setup() {
   Serial.begin(9600);
 
   analogReference(EXTERNAL);
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-  if (Serial.available() > 0) {
-    input = Serial.read();
-
-    if (input == 68){ // ASCII for "D"
-
-      // iterates through all temperature sensors
-      for (int i = 0; i < NUM_SENSORS; i++) {
-
-        // averages 10 values a specific sensor
-        int averageSum = 0;
-
-        for (int j = 0; j < 10; j++) {
-          averageSum += analogRead(TEMP_PINS[i]);
-        }
-        
-        Serial.print(convertCelsius(averageSum / 10));
-        Serial.print(';');
-      }
-      Serial.println();
-    }
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    sensorAvgs[i].begin();
   }
 }
 
+void loop() {
+
+  // checks if new data is being requested
+  if (Serial.available() > 0) {
+    input = Serial.read();
+    
+    if (input == 68) { // ASCII for "D"
+      outputValues = true;
+    }
+    if (input == 82) { // ASCII for "R"  prints raw read average
+      for (int i = 0; i < NUM_SENSORS; i++) {
+        Serial.print(sensorAvgs[i].getAvg());
+        Serial.print(';');
+      }
+      Serial.println('\r');
+    }
+    if (input == 65) { // ASCII for "A" prints 5 readings from Sensor A ADC
+      for (int i = 0; i < 5; i++) {
+        Serial.println(analogRead(TEMP_PINS[0]));
+      }
+    }
+
+   input = 0;
+ }
+
+  // iterates through all temperature sensors
+    for (int i = 0; i < NUM_SENSORS; i++) {
+
+      // adds new reading to rolling average
+      for (int j = 0; j < READS_PER_CYCLE; j++) {
+        sensorAvgs[i].reading(analogRead(TEMP_PINS[i]));
+      }
+      
+      // prints temperature data if requested
+      if (outputValues) {
+        Serial.print(convertCelsius(sensorAvgs[i].getAvg()));
+        Serial.print(';');
+      }
+
+    }
+
+  // finishes line if printing and resets output flag
+  if (outputValues) {
+    Serial.println('\r');
+    outputValues = false;
+  }
+
+  // limits loop speed
+  delay(LOOP_DELAY_MS);
+}
+
 float convertCelsius(int reading) {
-  float mV = reading * (aref_voltage / 1024.0);
+  float mV = reading * (AREF_VOLTAGE / 1024.0);
   // Serial.print(mV);
   // Serial.println(" millivolts");
 
